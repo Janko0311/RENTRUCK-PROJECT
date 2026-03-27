3d015e0bc4f144b6fdffbca77f2f7ec8ea766a9a
Janko0311 <jankocheng0311@gmail.com>
2026-03-27 19:15:06 -0400

    Add files via upload

---
+++ RENTRUCK/form.js
@@ -0,0 +1,28 @@
+function openModal() {
+    document.getElementById("addCustomerModal").classList.add("active");
+    
+}
+
+function closeModal() {
+    document.getElementById("addCustomerModal").classList.remove("active");
+}
+
+
+function openResModal() {
+    document.getElementById("addResModal").classList.add("active");
+    
+}
+
+function closeResModal() {
+    document.getElementById("addResModal").classList.remove("active");
+}
+
+
+function openMiModal() {
+    document.getElementById("addMiModal").classList.add("active");
+    
+}
+
+function closeMiModal() {
+    document.getElementById("addMiModal").classList.remove("active");
+}
